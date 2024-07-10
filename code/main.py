3c1e7c96f3a27ff70c82fc35e28535912589c4a9
samin <samincgs@gmail.com>
2024-07-10 03:18:34 -0400

    first commit

---
+++ code/main.py
@@ -0,0 +1,22 @@
+from settings import *
+
+class Game:
+	def __init__(self):
+		pygame.init()
+		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+		pygame.display.set_caption('Sprout land')
+		self.clock = pygame.time.Clock()
+
+	def run(self):
+		while True:
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					pygame.quit()
+					sys.exit()
+  
+			dt = self.clock.tick() / 1000
+			pygame.display.update()
+
+if __name__ == '__main__':
+	game = Game()
+	game.run()
