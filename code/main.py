243c6babf3f4bfdc6c89b2d5bc86c9e36f8ef601
samin <samincgs@gmail.com>
2024-07-14 18:59:31 -0400

    restarting project

--- code/main.py
+++ code/main.py
@@ -1,13 +1,10 @@
 from settings import *
-from level import Level
 
 class Game:
 	def __init__(self):
 		pygame.init()
 		self.screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
-		pygame.display.set_caption('Pydew Valley')
 		self.clock = pygame.time.Clock()
-		self.level = Level()
 
 	def run(self):
 		while True:
@@ -17,7 +14,6 @@ class Game:
 					sys.exit()
 			
 			dt = self.clock.tick() / 1000
-			self.level.run(dt)
 			pygame.display.update()
 
 if __name__ == '__main__':
